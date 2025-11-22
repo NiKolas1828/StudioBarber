438b42a43981cab38fe70d64b32ecbb3ded241b8
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-22 18:11:09 -0500

    feat(ui): optimización de la generación de barberos en la UI

--- app/src/main/java/com/dsmovil/studiobarber/ui/screens/admin/barbers/ManageBarbersScreen.kt
+++ app/src/main/java/com/dsmovil/studiobarber/ui/screens/admin/barbers/ManageBarbersScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -156,10 +157,9 @@ private fun SuccessView(
         contentPadding = PaddingValues(bottom = 80.dp)
     ) {
         items(
-            count = state.barbers.size, // TODO: mirar la key
-        ) { barberId ->
-            val barber = state.barbers[barberId]
-
+            items = state.barbers,
+            key = { barber -> barber.id }
+        ) { barber ->
             if (barber.id == state.editingBarberId) {
                 AdminEditableCard(
                     initialName = barber.name,
