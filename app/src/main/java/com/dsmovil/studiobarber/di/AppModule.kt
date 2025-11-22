e5d529d643bf8f3b3d05561cc68916d554bba597
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-11-22 16:52:40 -0500

    feat(di): configuración para los nuevos repositorios y casos de uso de barberos

--- app/src/main/java/com/dsmovil/studiobarber/di/AppModule.kt
+++ app/src/main/java/com/dsmovil/studiobarber/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.dsmovil.studiobarber.di
 
-import com.dsmovil.studiobarber.data.repositories.AuthRepository
+import com.dsmovil.studiobarber.domain.repositories.AuthRepository
 import com.dsmovil.studiobarber.data.repositories.AuthRepositoryImpl
+import com.dsmovil.studiobarber.data.repositories.BarberRepositoryImpl
+import com.dsmovil.studiobarber.domain.repositories.BarberRepository
+import com.dsmovil.studiobarber.domain.usecases.DeleteBarberUseCase
+import com.dsmovil.studiobarber.domain.usecases.GetBarbersUseCase
 import com.dsmovil.studiobarber.domain.usecases.LoginUseCase
 import com.dsmovil.studiobarber.domain.usecases.LogoutUseCase
 import com.dsmovil.studiobarber.domain.usecases.RegisterUseCase
+import com.dsmovil.studiobarber.domain.usecases.UpdateBarberUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -17,7 +22,11 @@ import javax.inject.Singleton
 abstract class AppModule {
     @Binds
     @Singleton
-    abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl) : AuthRepository
+    abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindBarberRepository(barberRepositoryImpl: BarberRepositoryImpl): BarberRepository
 
     companion object {
         @Provides
@@ -36,6 +45,24 @@ abstract class AppModule {
         @Singleton
         fun provideLogoutUseCase(repository: AuthRepository): LogoutUseCase {
             return LogoutUseCase(repository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideGetBarbersUseCase(repository: BarberRepository): GetBarbersUseCase {
+            return GetBarbersUseCase(repository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideDeleteBarberUseCase(repository: BarberRepository): DeleteBarberUseCase {
+            return DeleteBarberUseCase(repository)
+        }
+
+        @Provides
+        @Singleton
+        fun provideUpdateBarberUseCase(repository: BarberRepository): UpdateBarberUseCase {
+            return UpdateBarberUseCase(repository)
         }
     }
 }
